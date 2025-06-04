729a319dc7db44ee65696eb9f7acfb4d057dea41
Matheus Isaias Alves de Melo <matheus.isaias@eduzz.com>
2025-06-03 23:21:53 -0300

    npm e modularização + importação de bibliotecas

---
+++ js-node/aula4/index.js
@@ -0,0 +1,24 @@
+const fs = require('fs').promises;
+const path = require('path');
+
+async function ReadDir(rootDir) {
+    rootDir = rootDir || path.resolve(__dirname);
+    const files = await fs.readdir(rootDir)
+    walk(files, rootDir)
+}
+
+async function walk(files, rootDir) {
+    for (let file of files) {
+        const fileFullPath = path.resolve(rootDir, file)
+        const stats = await fs.stat(fileFullPath);
+
+        if (stats.isDirectory()) {
+            ReadDir(fileFullPath);
+            continue;
+        }
+
+        console.log(file, stats.isDirectory())
+    }
+}
+
+ReadDir('../../');
